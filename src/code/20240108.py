2177a4636f6eb3cbaf07df34f935ce1bcdbd125a
wenqiang <quwenqiang@iscas.ac.cn>
2024-01-08 17:48:09 +0800

    update

---
+++ src/code/20240108.py
@@ -0,0 +1,16 @@
+
+
+my_list = [1, 2, 3, 4, 5]
+
+with open("output.txt", "w") as file:
+    for item in my_list:
+        file.write(str(item) + "\n")
+
+
+
+
+with open("output.txt", "r") as file:
+    processed_files = file.read().splitlines()
+
+print(processed_files)
+# processed_files = [int(item) for item in processed_files]
