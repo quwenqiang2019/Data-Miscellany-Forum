798949e3521c2c6117fcc9713571a6b0b9f7cbda
wenqiang <quwenqiang@iscas.ac.cn>
2024-03-14 17:43:29 +0800

    feat():update code

--- src/code/test.py
+++ src/code/test.py
@@ -1,14 +1,12 @@
+def count_lines(filename):
+    with open(filename, 'r') as file:
+        line_count = sum(1 for line in file)
+    return line_count
 
-a = 3
-def t1():
-    a += 1
-    print(a)
+filename = 'output1.txt'  # 将 'your_file.txt' 替换为您的txt文件路径
 
-
-
-
-if __name__ == '__main__':
-    t1()
+line_count = count_lines(filename)
+print("Total number of lines in the file:", line_count)
 
 
 
