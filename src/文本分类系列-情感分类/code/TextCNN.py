d4ac4e956a02483a3b299d04590d8746d7cad149
wenqiang <quwenqiang@iscas.ac.cn>
2024-05-06 18:11:04 +0800

    feat():update code

--- src/文本分类系列-情感分类/code/TextCNN.py
+++ src/文本分类系列-情感分类/code/TextCNN.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Embedding, LSTM, Dense, Bidirectional
+from keras.models import Sequential, Model
+from keras.layers import Embedding, LSTM, Dense, Bidirectional, Input, Dropout, Attention, Flatten, Convolution1D, MaxPool1D
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-
+from keras.layers import concatenate
 
 reviews = pd.read_csv('E:\data\IMDB Dataset.csv')
 print(reviews.head())
 
-# 将影评情感转为0和1的数值，并将影评和情感转化为numpy数组，最后划分训练集和测试集。
 reviews['sentiment'] = np.where(reviews['sentiment'] == 'positive', 1, 0)
 sentences = reviews['review'].to_numpy()
 print(sentences)
@@ -28,31 +27,38 @@ print("Testing Data Input Shape: ", X_test.shape)
 print("Testing Data Output Shape: ", y_test.shape)
 
 
-# 构建分词器，构建单词索引，将字符串转化成整数索引组成的列表
 vocab_size = 10000
-oov_tok = "<OOV>" # 参数oov_tok指定一个特殊的标记，用于表示在词汇表中未出现的词语。
+oov_tok = "<OOV>"
 tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(X_train)
 train_sequences = tokenizer.texts_to_sequences(X_train)
-# 将整数列表转化为二维数值张量，相同的操作对测试集再执行一遍。
+
+
 sequence_length = 200
 train_padded = pad_sequences(train_sequences, maxlen=sequence_length, padding='post', truncating='post')
 test_sequences = tokenizer.texts_to_sequences(X_test)
 test_padded = pad_sequences(test_sequences, maxlen=sequence_length, padding='post', truncating='post')
 
-
 embedding_dim = 16
 lstm_out = 32
-model = Sequential()
-# 定义Embedding词嵌入层。这是因为深度学习模型只能处理数值张量类型的数据。16表示的是词向量维度。
-model.add(Embedding(vocab_size, embedding_dim, input_length=sequence_length))
-# 定义一个双向LSTM层，定义一个relu激活的全连接层，定义一个使用sigmoid激活的输出层。
-model.add(Bidirectional(LSTM(lstm_out)))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+inputs = Input(name='inputs',shape=[sequence_length], dtype='float64')
+## 词嵌入使用预训练的词向量
+layer = Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
+## 词窗大小分别为3,4,5
+cnn1 = Convolution1D(256, 3, padding='same', strides = 1, activation='relu')(layer)
+cnn1 = MaxPool1D(pool_size=4)(cnn1)
+cnn2 = Convolution1D(256, 4, padding='same', strides = 1, activation='relu')(layer)
+cnn2 = MaxPool1D(pool_size=4)(cnn2)
+cnn3 = Convolution1D(256, 5, padding='same', strides = 1, activation='relu')(layer)
+cnn3 = MaxPool1D(pool_size=4)(cnn3)
+# 合并三个模型的输出向量
+cnn = concatenate([cnn1,cnn2,cnn3], axis=-1)
+flat = Flatten()(cnn)
+drop = Dropout(0.2)(flat)
+main_output = Dense(1, activation='sigmoid')(drop)
+model = Model(inputs=inputs, outputs=main_output)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# 模型自动判断迭代次数，防止过拟合。
 checkpoint_filepath = os.getcwd()
 model_checkpoint_callback = ModelCheckpoint(filepath=checkpoint_filepath, save_weights_only=False, monitor='val_loss', mode='min', save_best_only=True)
 callbacks = [EarlyStopping(patience=2), model_checkpoint_callback]
