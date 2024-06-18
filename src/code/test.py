06346e4ffaf39e3233665ddb009e0198b8b84c1a
wenqiang <quwenqiang@iscas.ac.cn>
2024-06-18 18:12:27 +0800

    feat():update code

--- src/code/test.py
+++ src/code/test.py
@@ -1,15 +1,15 @@
-# a = ['a', 'b', 'c', 'd']
-# b = ['1', '2', '3', '4']
-#
-# res = list(zip(a, b))
-# print(res)
-#
-#
-# origin = zip(*res)  # #前面加*号，事实上*号也是一个特殊的运算符，叫解包运算符
-# print(list(origin))
-#
-# s = ["flower","flow","flight"]
-# print(list(zip(*s)))
+a = ['a', 'b', 'c', 'd']
+b = ['1', '2', '3', '4']
+
+res = list(zip(a, b))
+print(res)
+
+
+origin = zip(*res)  # #前面加*号，事实上*号也是一个特殊的运算符，叫解包运算符
+print(list(origin))
+
+s = ["flower","flow","flight"]
+print(list(zip(*s)))
 #
 #
 # s = "A man, a plan, a canal: Panama"
@@ -57,14 +57,48 @@
 # removeDuplicates(nums)
 
 
-def twoSum(nums: list[int], target: int) -> list[int]:
-    hashtable = dict()
-    for i, num in enumerate(nums):
-        print(hashtable)
-        if target - num in hashtable:
-            print(hashtable[target - num])
-            return [hashtable[target - num], i]
-        hashtable[num] = i
-    return []
+# def twoSum(nums: list[int], target: int) -> list[int]:
+#     hashtable = dict()
+#     for i, num in enumerate(nums):
+#         print(hashtable)
+#         if target - num in hashtable:
+#             print(hashtable[target - num])
+#             return [hashtable[target - num], i]
+#         hashtable[num] = i
+#     return []
+#
+# twoSum([2,10,7,11,15], 9)
 
-twoSum([2,10,7,11,15], 9)
+
+# def romanToInt(s: str) -> int:
+#     hashtable = {
+#         "I": 1,
+#         "V": 5,
+#         "X": 10,
+#         "L": 50,
+#         "C": 100,
+#         "D": 500,
+#         "M": 1000,
+#         "a": 4,
+#         "b": 9,
+#         "c": 40,
+#         "d": 90,
+#         "e": 400,
+#         "f": 900
+#     }
+#
+#     res = 0
+#     s = s.replace("IV", "a")
+#     s = s.replace("IX", "b")
+#     s = s.replace("XL", "c")
+#     s = s.replace("XC", "d")
+#     s = s.replace("CD", "e")
+#     s = s.replace("CM", "f")
+#
+#     for i, v in enumerate(s):
+#         temp = hashtable[v]
+#         res = res + temp
+#
+#     return res
+#
+# romanToInt("III")
